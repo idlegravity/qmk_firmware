7c7dbec23a3c900fa7a6cfdcb6a2c0ef162e717b
idlegravity <idlegravity@gmail.com>
2019-09-06 09:11:03 -0600

    Added custom Pok3r3sque layout.

---
+++ keyboards/bpiphany/pegasushoof/2015/keymaps/pok3r3sque/config.h
@@ -0,0 +1,10 @@
+#pragma once
+
+/* overridden settings: */
+
+#undef MANUFACTURER
+#define MANUFACTURER    idlegravity
+#undef PRODUCT
+#define PRODUCT         Pok3r3sque
+#undef DESCRIPTION
+#define DESCRIPTION     QMK on the Pegasus Hoof
